1963ef7190ece176cf9671aee726e6a7e3fac298
Edison1234567 <31074447+Edison1234567@users.noreply.github.com>
2017-12-04 09:50:14 -0800

    added guessgame

---
+++ Edison Shapiro - Guessgame.py
@@ -0,0 +1,7 @@
+import random  # this should be on line 1
+# REMEMBER, INPUTS FROM USERS ARE ALWAYS (!!!)
+# OF TYPE STRING!!!!
+# Random Number
+print(random.randint(1,50))
+
+
--- Notes.py
+++ Notes.py
@@ -1,38 +1,38 @@
-# print("Hello world")
-# # This is a new note
-#
-# # Edison Shapiro
-#
-# a = 4
-# b = 3
-#
-# print(3 + 5)
-# print(5 - 3)
-# print(3 * 5)
-# print(6 / 2)
-# print(3 ** 2)
-#
-# print()
-# print("Try to figure out how this works")
-# print(13 % 5)
-#
-# # the "%" sign is a modulus. It finds the remainder.
-#
-# car_name = "The Edison Mobile"
-# car_type = "BMW"
-# car_cylinders = 8
-# car_mpg = 5000.9
-#
-# print("I have a car called %s. It's pretty nice." % car_name)
-# print("I have a car called %s. It's a %s." % (car_name, car_type)) # watch the order
-#
-# # Here is where we get a little fancy
-# print("What is your name?")
-# name = input(">_ ")
-# print("Hello %s." % name)
-#
-# age = input("How old are you?")
-# print("%s?! That's really old. You belong in a retirement home." % age)
+print("Hello world")
+# This is a new note
+
+# Edison Shapiro
+
+a = 4
+b = 3
+
+print(3 + 5)
+print(5 - 3)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 2)
+
+print()
+print("Try to figure out how this works")
+print(13 % 5)
+
+# the "%" sign is a modulus. It finds the remainder.
+
+car_name = "The Edison Mobile"
+car_type = "BMW"
+car_cylinders = 8
+car_mpg = 5000.9
+
+print("I have a car called %s. It's pretty nice." % car_name)
+print("I have a car called %s. It's a %s." % (car_name, car_type)) # watch the order
+
+# Here is where we get a little fancy
+print("What is your name?")
+name = input(">_ ")
+print("Hello %s." % name)
+
+age = input("How old are you?")
+print("%s?! That's really old. You belong in a retirement home." % age)
 
 # Functions
 
@@ -104,6 +104,7 @@ def happy_bday(name):
     print("Happy Birthday dear " + name + ',')
     print("Happy Birthday to you" + ',')
 
+
 happy_bday("Edison")
 
 # Loops
@@ -116,6 +117,20 @@ while a < 10:
     print(a)
     a += 1
 
-# Random
+# Random Numbers
 import random  # this should be on line 1
 print(random.randint(0,10))
+
+
+# Recasting
+c = '1'
+print(c == 1)   # we have a string and an int
+print(int(c) == 1)
+print(c == str(1))
+
+
+# Comparisons
+
+print(1 == 1)   # Use a double equal sign
+print(1 != 2)   # 1 is not equal to 2
+print(not False)
