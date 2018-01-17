de7216f9ed1d4b56b320ad7e94d2b208f95090fd
Edison1234567 <31074447+Edison1234567@users.noreply.github.com>
2018-01-17 09:53:14 -0800

    finished Lucky 7's

--- Edison Shapiro - Lucky 7's.py
+++ Edison Shapiro - Lucky 7's.py
@@ -1,8 +1,21 @@
 import random  # this should be on line 1
-dice1 = random.randint(1,6)
-dice2 = random.randint(1,6)
-print(random.randint(1,6))
-print(random.randint(1,6))
-total = dice1 + dice2
-print("the total is %d" % total)
+money = 15
+High_Score = 0
+Best_round = 0
+Rounds = 0
 
+while money > 0:
+    print()
+    print("New round")
+    money -= 1
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    print(random.randint(1,6))
+    print(random.randint(1,6))
+    total = dice1 + dice2
+    print("the total is %d" % total)
+    if total == 7:
+        print("You gained $5. You win this round!")
+        money += 5
+    else:
+        print("you have lost $1. You Lose this round!")
