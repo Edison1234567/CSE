19948a0e30d9b8b9764bf66d9ed932136f824d80
Edison1234567 <31074447+Edison1234567@users.noreply.github.com>
2018-01-24 09:50:42 -0800

    add random word selection

--- Edison Shapiro - Hangman.py
+++ Edison Shapiro - Hangman.py
@@ -1,16 +1,17 @@
+import random
 """
 This is a guide of how to
 make hangman
 
 1. Make a word bank - 10 items
 2. Select a random item to guess
-3. Hide the word (use *)
+3. Add user input to a list of letters_guessed
 4. Reveal Letters based on input
 5. Create win and lose conditions
 """
 # Adding things to a list
 word_bank = ['Pokemon', "Polygons", "FootBall", "VolleyBall", "BasketBall", "Tennis", "Soccer", "Computer", "Justice",
              "Tigers"]
-import random  # this should be on line 1
-print(random.randint(1,10))
-print(word_bank[5])
+the_answer = random.choice(word_bank)
+print(the_answer)
+letters_guessed = 10
