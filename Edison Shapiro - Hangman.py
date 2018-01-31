fd81a0a58b458f18fb176262ace53800fc6f74e0
Edison1234567 <31074447+Edison1234567@users.noreply.github.com>
2018-01-31 09:50:51 -0800

    added remembered list

--- Edison Shapiro - Hangman.py
+++ Edison Shapiro - Hangman.py
@@ -16,14 +16,22 @@ the_answer = random.choice(word_bank)
 print("the guessed word is %s" % str(the_answer))
 guesses_left = 10
 guess = "*"
+letters_guessed = []
 while guess != 'quit':
+    output = []
+    for letter in the_answer:
+        if letter in letters_guessed:
+            output.append(letter)
+    else:
+        output.append("*")
+        print(output)
+
+
     guess = input("Guess a letter: ")
     if guess in the_answer:
         print("you have won this guess")
     else:
         print("wrong guess try again")
         guesses_left -= 1
-letters_guessed = []
-for num in range(10):
-    print(num + 1)
-   
+    letters_guessed.append(guess)
+    print(letters_guessed)
