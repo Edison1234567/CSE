74a0afd22ff7b6455b1b067437d2c80acc5811b7
Edison1234567 <31074447+Edison1234567@users.noreply.github.com>
2018-02-22 09:48:04 -0800

    added car

--- Class Notes.py
+++ Class Notes.py
@@ -1,6 +1,6 @@
 # Defining a class
 class Cheeseburger(object):
-    def __init__(self, patty_type):  # Two underscores before and after
+    def __init__(self, patty_type, cheese):  # Two underscores before and after
         self.patty = patty_type
         self.cheese = cheese
         self.eaten = False
@@ -29,10 +29,47 @@ print(burger2.eaten)
 
 
 class Car(object):
-    def __init__(self, color, num_of_doors, hp):
+    def __init__(self, name, color, num_of_doors, hp):
         self.color = color
         self.doors = num_of_doors
         self.running = False
         self.HP = hp
         self.passengers = 0
         self.name = name
+        self.air_conditioning = True
+
+    def turn_on(self):
+        if self.running:
+            print("Nothing Happens.")
+        else:
+            self.running = True
+            print("The car starts.")
+
+    def move_forward(self):
+        if self.running:
+            print("You move forward")
+        else:
+            print("Nothing Happens")
+
+    def turn_off(self):
+        if self.running:
+            self.running = False
+            print("You turn the car off")
+        else:
+            print("The car is already off")
+
+    def go_for_drive(self, passengers):
+        print("%d passengers get in." % passengers)
+        self.passengers = passengers
+        self.turn_on()
+        self.move_forward()
+        self.move_forward()
+        self.move_forward()
+        self.turn_off()
+        print("%d passengers get out." % passengers)
+        self.passengers = 0
+
+
+my_car = Car("Edison", "blue", 4, 4000)
+
+my_car.go_for_drive(5)
