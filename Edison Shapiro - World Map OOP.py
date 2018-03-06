3c7a243363dba555a5cc444162527c2442eaa85c
Edison1234567 <31074447+Edison1234567@users.noreply.github.com>
2018-03-06 09:41:45 -0800

    finished new world map

--- Edison Shapiro - World Map OOP.py
+++ Edison Shapiro - World Map OOP.py
@@ -8,8 +8,8 @@ class Room(object):
         self.description = description
 
     def move(self, direction):
-                global current_node
-                current_node = globals()[getattr(self, direction)]
+        global current_node
+        current_node = globals()[getattr(self, direction)]
 
 
 tower = Room("Tower", None, 'rocks', None, 'shore', 'Defense Building')
@@ -20,7 +20,7 @@ point_b = Room("Point_B", 'falls', None, None, 'castle', 'Flag')
 falls = Room("Falls", None, 'grotto', 'point_b', None, 'Waterfall')
 grotto = Room("Grotto", 'cave', 'point_c', None, 'falls', 'Small Cave')
 point_c = Room("Point_c", 'cave', None, None, 'grotto', 'Flag')
-cave = Room("Cave", 'Meadow', 'point_c', 'grotto', 'crash','Dark Pathway')
+cave = Room("Cave", 'Meadow', 'point_c', 'grotto', 'crash', 'Dark Pathway')
 meadow = Room("Meadow", None, None, 'cave', 'crash', 'Grassland')
 crash = Room("crash", None, 'meadow', 'cave', 'ketch', 'Ship Crash')
 ketch = Room("Ketch", 'point_a', 'crash', 'rocks', None, 'Ship')
@@ -29,13 +29,13 @@ rocks = Room("rocks", 'ketch', None, 'passage', 'tower', 'Stones')
 passage = Room("passage", 'rocks', None, 'castle', None, 'Through Way')
 
 current_node = tower
-directions = ['north', 'south','east', 'west']
+directions = ['north', 'south', 'east', 'west']
 short_directions = ['n', 's', 'e', 'w']
 
 while True:
 
-    print(current_node.name)   # Change
-    print(current_node.description)   # Change
+    print(current_node.name)
+    print(current_node.description)
     command = input('>_').lower().strip()
     if command == 'quit':
         quit(0)
@@ -44,7 +44,7 @@ while True:
         command = directions[pos]
     if command in directions:
         try:
-            print(current_node)  # Change
+            current_node.move(command)
         except KeyError:
             print("You cannot go this way")
     else:
