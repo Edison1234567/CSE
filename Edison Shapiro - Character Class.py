9f6ea13a54cfd7168da3cd052005caf8911f9e6b
Edison1234567 <31074447+Edison1234567@users.noreply.github.com>
2018-03-12 16:03:26 -0700

    added instance varables

--- Edison Shapiro - Character Class.py
+++ Edison Shapiro - Character Class.py
@@ -1,2 +1,11 @@
 class Character(object):
-    def __init__(self, name):
+    def __init__(self, name, attack, death, take_damage, status_effect):
+        self.name = name
+        self.health = 120
+        self.attack = attack
+        self.death = death
+        self.take_damage = take_damage
+        self.paralyze = status_effect
+        self.dialogue = True
+
+    
