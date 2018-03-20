a69a12d603d5de08a6a6a5e6f961470bc7f9e588
Edison1234567 <31074447+Edison1234567@users.noreply.github.com>
2018-03-20 15:31:39 -0700

    fixed item coding

--- Edison Shapiro - Inheritance.py
+++ Edison Shapiro - Inheritance.py
@@ -1,13 +1,26 @@
 class Item(object):
-    def __init__(self, name):
+    def __init__(self, name, description):
         self.name = name
+        self.description = description
 
 
 class Weapon(Item):
-    def __init__(self, name):
-        super(Weapon, self).__init__(name)
+    def __init__(self, name, attack, description):
+        super(Weapon, self).__init__(name, description)
+        self.attack = attack
 
 
 class Sword(Weapon):
-    def __init__(self, name):
-        super(Sword, self).__init__(name)
+    def __init__(self, name, sharp_blade, attack, description):
+        super(Sword, self).__init__(name, attack, description)
+        self.sharp_blade = sharp_blade
+
+
+class Gun(Weapon):
+    def __init__(self, name, attack, description, ammo):
+        super(Gun, self).__init__(name, attack, description)
+        self.ammo = ammo
+
+
+class Pulse_Rifle(Gun):
+    
