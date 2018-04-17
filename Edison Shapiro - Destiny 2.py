a9189d9892ab94da017ea8808aafa1de70a9f686
Edison1234567 <31074447+Edison1234567@users.noreply.github.com>
2018-04-17 09:50:07 -0700

    coded begining portion in controller

--- Edison Shapiro - Destiny 2.py
+++ Edison Shapiro - Destiny 2.py
@@ -24,9 +24,6 @@ class Character(object):
 
 player = Character("Test", 0, 0, 0, 0)
 player.use_armor(2)
-player.death()
-player.take_damage(5)
-player.attack()
 
 
 class Room(object):
@@ -303,6 +300,15 @@ while True:
     elif command[:7] == "pick up":
         item = command[8:]
         print("You have picked up the %s" % item)
+    elif command[:7] == player.attack():
+        attack = command[8:]
+        print("You have placed a %s on a fallen" % attack)
+    elif command[:7] == player.take_damage(5):
+        take_damage = command[8:]
+        print("The fallen has made you %s" % take_damage)
+    elif command[:7] == player.death():
+        death = command[8:]
+        print("The fallen caused you to take a %s" % death)
     else:
         print("Command not recognized")
         
